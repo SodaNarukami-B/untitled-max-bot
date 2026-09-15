0ab24fd94d407aa9fcbb68ad1ff897b684d6a52e
SodaNarukami-B <207603195+SodaNarukami-B@users.noreply.github.com>
2026-09-15 19:55:24 +0500

    routing update

--- core.py
+++ core.py
@@ -2,66 +2,98 @@ from __future__ import annotations
 from fastapi import FastAPI, Request, HTTPException
 import uvicorn
 
+app = FastAPI()
 
-app = FastAPI();
+supported_types = ["newMessage", "callbackQuery"]
+supported_commands = ["/echo"];
 
-class Command:
+class Text_Command:
     def __init__(self, command: str, params: list[str]):
-        self.command = command;
-        self.params = params;
+        self.command = command
+        self.params = params
 
     @classmethod
-    def parse(cls, raw_text: str) -> Command:
-        separated = raw_text.strip().split();
+    def parse(cls, raw_text: str) -> Text_Command:
+        separated = raw_text.strip().split()
 
         if not separated:
-            return cls(command="", params=[]);
+            return cls(command="", params=[])
 
         return cls(command=separated[0], params=separated[1:])
 
 
+def handle_newMessage(data: dict) -> dict | int:
+    # Paylaod
+    payload = data.get("payload")
+    if not isinstance(payload, dict):
+        return 1  # User mistake / invalid structure
+
+    # Dicts with chat_id and user_id
+    chat = payload.get("chat")
+    frm = payload.get("from")
+
+    # Message text
+    text = payload.get("text")
+
+    if not isinstance(chat, dict) or not isinstance(frm, dict) or not text:
+        return 1
+
+    # Requied fields
+    chat_id = chat.get("chatId")
+    user_id = frm.get("userId") # XXX: I don't think we need it
+
+    if not chat_id or not user_id:
+        return 1
+
+
+    # Command parsing
+    command = Text_Command.parse(text)
+    if not command.command:
+        return 1
+
+    if command.command not in supported_commands:
+        return 1;
+
+    match command.command:
+        case "/echo":  # Echo
+            return {
+                "chatId": chat_id,
+                "text": " ".join(command.params)
+            }
+        case _:
+            return 2
 
 @app.post("/core")
 async def handle_webhook(request: Request):
-    data = await request.json();
+    try:
+        data = await request.json()
+        if not isinstance(data, dict):
+            raise HTTPException(status_code=400, detail="Invalid data")
 
-    uid = data.get("user_id");
-    cid = data.get("chat_id");
-    text = data.get("text");
+    except Exception:
+        raise HTTPException(status_code=400, detail="Invalid data")
 
-    if uid is None or cid is None or text is None:
-        raise HTTPException(
-            status_code=400,
-            detail="Unexpected message"
-        );
+    # Type check
+    event_type = data.get("type")
 
-    command = Command.parse(text);
-    if command.command == "" and command.params == []:
-        raise HTTPException(
-            status_code=400,
-            detail="Invalid command"
-        );
-    print(f"Command received:\n\tcommand='{command.command}'\n\tparams={command.params}");
+    # route by type
+    match event_type:
+        case "newMessage":
+            response = handle_newMessage(data)
 
-    match command.command:
-        case "/find":
-            return {
-                    "method": "sendMessage",
-                    "chat_id": cid,
-                    "text": f"[Find command] called: text={command.params[0]}"
-            };
+            if response == 1:
+                raise HTTPException(status_code=400, detail="Invalid syntax")
+            if response == 2:
+                raise HTTPException(status_code=500, detail="Server error")
+
+            return response
+
+        case "callbackQuery":
+            return {"status": "ok"}
+
         case _:
-            return {
-                    "method": "sendMessage",
-                    "chat_id": cid,
-                    "text": "[No command] called"
-            };
+            raise HTTPException(status_code=404, detail="Invalid type")
 
-
-    print("Received:", data);
-
-    return {"code": 1};
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080);
-
+    uvicorn.run(app, host="127.0.0.1", port=8080)
